2416f03d1121e737ecc3e0d15cdbcebe344d7fa2
snehalk1607 <snehalkalmegh07@gmail.com>
2024-03-17 21:34:41 +0100

    feat: testing commits to new branch

--- constants/labels-resource.ts
+++ constants/labels-resource.ts
@@ -8,6 +8,7 @@ export const LabelsResource ={
     'TASKLIST_ADD_EDIT_SCREEN_DUE_DATE': 'Select Due Date',
     'TASKLIST_ADD_EDIT_SCREEN_PRIORITY':'Select Priority',
     'TASKLIST_ADD_EDIT_SCREEN_SUBMIT':'Add Task',
-    'TASKLIST_ADD_EDIT_SCREEN_HEADING':'Add New Task'
+    'TASKLIST_ADD_EDIT_SCREEN_HEADING':'Add New Task',
+    'TEMP_TEXT': 'TEMP TEXT'
 
 }
